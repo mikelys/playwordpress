2abc43872827b6cf9b44222c96edfb4a7dbfeb5e
mike <glarky@gmail.com>
2021-04-18 19:22:24 -0400

    list pagination nav

--- wp-content/plugins/paginate-nav/paginate-nav.php
+++ wp-content/plugins/paginate-nav/paginate-nav.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 /**
  * Plugin Name: Pagination Nav Plugin
  * Plugin URI: http://wordpress/pagination-nav
@@ -8,10 +8,52 @@
  * Author URI: http://glarky.com
  */ 
 
-add_action( 'the_content', 'my_thank_you_text' );
+class list_Pagination_Nav extends WP_Widget {
+    function __construct(){
+        $widget_ops = array( 
+            'classname' => 'list_pagination_nav', 
+            'description' => 'display pagination nav'
+        );
+       parent::__construct( 'list_paginationi_nav', 'List Pagination', $widget_ops ); 
+    }
+    
+    function widget( $args, $instance ) {
+        extract($args, EXTR_SKIP);
+        
+        echo $before_widget;
+        $title = empty($instance['title']) ? ' ' : apply_filters( 'widget_title', $instance['title'] );
+        
+        if (!empty($title))
+           echo $before_title . $title . $after_title;;
 
-function my_thank_you_text( $content ) {
-    return $content = '<p>Thank you for reading!</p>';
+        // WIDGET code goes here
+        ?>
+          <ul class="list-pagination-class">
+          <?php //create and run custom loop
+            $ourCurrentPage = get_query_var('paged'); 
+            $htmlPosts = new WP_Query(array(
+              'category_name' => 'html',  
+              'posts_per_page' => -1,
+              'paged' => $ourCurrentPage 
+            ));
+            while ($htmlPosts->have_posts()) : $htmlPosts->the_post();
+           ?>
+            <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+            <?php
+                  endwhile;
+                  wp_reset_postdata(); 
+            ?>
+          </ul>
+        <?php
+         echo $after_widget;   
+         
+    }
+}
+
+add_action( 'widgets_init', 'list_pagination_nav' );
+
+function list_pagination_nav() {
+  register_widget( 'list_Pagination_Nav' );
 }
 
 
